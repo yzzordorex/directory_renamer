272004197bc5f3580b2275f1c78aacc342eaa5f4
Aimee <aimee@barbariangroup.com>
2019-02-20 14:54:12 -0700

    Thing to rename directories how I prefer

---
+++ rename.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+require 'fileutils'
+include FileUtils
+base_path = './*'
+all_files = Dir[base_path].select {|f| File.directory? f }
+all_files.each do |file|
+  filepath = file
+  file[0...2] = ''
+  puts file
+  parts = file.split(" - ")
+  if parts.length != 2 
+    puts "skipping #{filepath}"
+  else
+    artist = parts[0]
+    album_year = parts[1]
+    year_arr = parts[1].split("(")
+    album = year_arr[0]
+    yearch = year_arr[1]
+    year = yearch.chomp(")")
+    mkdir_p("./#{artist}/#{year} - #{album}")
+    new_dir =  "./#{artist}/#{year} - #{album}"
+    puts "./#{artist}/#{year} - #{album}"
+    puts "Copying..."
+    FileUtils.cp_r("#{filepath}/.", new_dir)
+  end
+end
+puts "End"
+
